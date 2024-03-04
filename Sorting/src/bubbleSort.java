e2390f3be584e7b57d68091c2ce294e07d124ed7
Yogesh <yogeshmind222@gmail.com>
2024-03-04 21:17:42 +0530

    bubbleSort problem

---
+++ Sorting/src/bubbleSort.java
@@ -0,0 +1,21 @@
+public class bubbleSort {
+    static int[] bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        for (int x : bubbleSort(arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
